f7a1eecba6de74d8af36c1b5f91ba2c6cfa49515
donjoe <giumontanaro@gmail.com>
2018-08-02 20:19:26 +0200

    render markers for each place

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,23 +1,35 @@
 import React, { Component } from 'react';
-import { withGoogleMap, GoogleMap } from 'react-google-maps';
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
 class Map extends Component {
    render() {
-   const GoogleMapExample = withGoogleMap(props => (
+    const { places } = this.props;
+    const GoogleMapExample = withGoogleMap(props => (
       <GoogleMap
-        defaultCenter = { { lat: 40.756795, lng: -73.954298 } }
-        defaultZoom = { 13 }
+        defaultCenter = { { lat: 40.756795, lng: 17.954298 } }
+        defaultZoom = { 8 }
       >
+        {places.map(place => (
+          <Marker 
+            key={place.venue.id} 
+            position={{
+                lat: place.venue.location.lat,
+                lng: place.venue.location.lng 
+            }}/>
+    
+        ))}
+        
       </GoogleMap>
-   ));
-   return(
+    ));
+    return(
       <div>
         <GoogleMapExample
           containerElement={ <div style={{ height: `500px`, width: '500px' }} /> }
           mapElement={ <div style={{ height: `100%` }} /> }
         />
       </div>
-   );
+    );
    }
-};
+}
+
 export default Map;
