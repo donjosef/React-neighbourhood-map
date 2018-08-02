c4380ba816e0d48df5917dab3760ad7181b82024
donjoe <giumontanaro@gmail.com>
2018-08-02 22:39:11 +0200

    use onMarkerClick as props

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,25 +2,25 @@ import React, { Component } from 'react';
 import { withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
 
 class Map extends Component {
+    
    render() {
-    const { places, center } = this.props;
+       
+    const { places, center, onMarkerClick } = this.props;
+       
     const GoogleMapExample = withGoogleMap(props => (
       <GoogleMap
         defaultCenter = { center }
         defaultZoom = { 9 }
       >
-        {places.map(place => (
+        {places.map((place, index) => (
           <Marker 
+            onClick={ () => onMarkerClick(index, {lat: place.venue.location.lat, lng: place.venue.location.lng}) }
             key={place.venue.id} 
             position={{
                 lat: place.venue.location.lat,
                 lng: place.venue.location.lng 
             }}
-          >
-       
-        
-        </Marker>
-        
+          ></Marker>     
     
         ))}
         
@@ -29,7 +29,7 @@ class Map extends Component {
     return(
       <div>
         <GoogleMapExample
-          containerElement={ <div style={{ height: `500px`, width: '500px' }} /> }
+          containerElement={ <div style={{ height: `100vh`}} /> }
           mapElement={ <div style={{ height: `100%` }} /> }
         />
       </div>
