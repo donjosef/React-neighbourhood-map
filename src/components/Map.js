4d43588a42998420f1832d14ef304127cc15db0d
donjoe <giumontanaro@gmail.com>
2018-08-03 12:21:35 +0200

    return CompositeMap from Map component with all the props

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,39 +1,20 @@
 import React, { Component } from 'react';
-import { withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
+import CompositeMap from './CompositeMap'
 
 class Map extends Component {
     
    render() {
+       const { places, center, onMarkerClick, selectedIndex } = this.props; //from state of App.js
        
-    const { places, center, onMarkerClick } = this.props;
-       
-    const GoogleMapExample = withGoogleMap(props => (
-      <GoogleMap
-        defaultCenter = { center }
-        defaultZoom = { 9 }
-      >
-        {places.map((place, index) => (
-          <Marker 
-            onClick={ () => onMarkerClick(index, {lat: place.venue.location.lat, lng: place.venue.location.lng}) }
-            key={place.venue.id} 
-            position={{
-                lat: place.venue.location.lat,
-                lng: place.venue.location.lng 
-            }}
-          ></Marker>     
-    
-        ))}
-        
-      </GoogleMap>
-    ));
-    return(
-      <div>
-        <GoogleMapExample
-          containerElement={ <div style={{ height: `100vh`}} /> }
-          mapElement={ <div style={{ height: `100%` }} /> }
-        />
-      </div>
-    );
+       return(
+          <div>
+            <CompositeMap
+              {...this.props}
+              containerElement={ <div style={{ height: `100vh`}} /> }
+              mapElement={ <div style={{ height: `100%` }} /> }
+            />
+          </div>
+       );
    }
 }
 
