a81c5a7e435bb0aa12db73857a3b653457eec9d8
donjoe <giumontanaro@gmail.com>
2018-08-03 17:11:18 +0200

    create component for content fo infowindow

---
+++ src/components/InfoWindowContent.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import * as PlacesDataAPI from '../PlacesDataAPI'
+
+class InfoWindowContent extends Component {
+    state = {
+        isLoading: true
+    }
+
+
+
+    render() {
+        return ( 
+            <div>
+             Content
+            </div>
+    
+    
+        )
+    
+    }
+}
+export default InfoWindowContent
