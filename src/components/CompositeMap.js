1adf3f8ede67bf8815b38e7713a5957634c0c182
donjoe <giumontanaro@gmail.com>
2018-08-03 12:35:34 +0200

    remove Component from import

--- src/components/CompositeMap.js
+++ src/components/CompositeMap.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React from 'react'
 import { withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
 
 const CompositeMap = withGoogleMap(props => { 
