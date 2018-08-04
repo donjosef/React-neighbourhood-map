d0d32d1cf96dab185fcf1a2d788b0d074d31114e
donjoe <giumontanaro@gmail.com>
2018-08-04 09:02:42 +0200

    return only the ul

--- src/components/ListPlaces.js
+++ src/components/ListPlaces.js
@@ -6,28 +6,16 @@ class ListPlaces extends Component {
     render() {
         const { places, onItemClick } = this.props;
         return (
-            <div className='sideBar'>
-               <div className='filterBarContainer'>
-                <label className='labelForFilter'>
-                 Filter Places you are interested in:
-                 <input className='filterBar' type='text'/>
-                </label>
-               </div>
-               <nav>
-                 <ul className='listPlaces'>
-                 {places.map((place, idx) => (
-                   <li 
+            <ul className='listPlaces'>
+                {places.map((place, idx) => (
+                  <li 
                      key={place.venue.id} 
                      onClick={() => onItemClick(idx, {lat: place.venue.location.lat, lng: place.venue.location.lng})}
                     >
                      {place.venue.name}
                    </li>
                   ))}
-                 </ul>
-               </nav>
-            </div>
-        
-        
+            </ul>
         )
     }
 }
