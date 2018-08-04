6cd3dc1e039a90e3bbd2bf22a88ef1e7674f03b2
donjoe <giumontanaro@gmail.com>
2018-08-04 16:51:43 +0200

    added component Header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header(props) {
+    return (
+      <header className='header'>
+        <h1>Best Places in Puglia</h1>
+      </header>
+    
+    );
+}
+
+export default Header
