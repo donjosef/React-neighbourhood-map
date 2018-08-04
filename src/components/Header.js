5b1763bc872df71d27a48c8a418038934e1a1a99
donjoe <giumontanaro@gmail.com>
2018-08-04 20:25:32 +0200

    added buttonto toggle the side menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,11 @@ import React from 'react'
 function Header(props) {
     return (
       <header className='header'>
+        <button onClick={props.onToggleMenu}>
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
         <h1>Best Places in Puglia</h1>
       </header>
     
