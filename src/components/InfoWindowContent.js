6a1ca057a06ca601194989b99e0bb1b9e864dd7d
donjoe <giumontanaro@gmail.com>
2018-08-04 11:31:41 +0200

    set the content of infowindow based on the response of foursquare details

--- src/components/InfoWindowContent.js
+++ src/components/InfoWindowContent.js
@@ -3,15 +3,49 @@ import * as PlacesDataAPI from '../PlacesDataAPI'
 
 class InfoWindowContent extends Component {
     state = {
-        isLoading: true
+        loaded: false,
+        error: false,
+        selectedPlace: {}
     }
 
 
+//this component is mounted after the click on marker. See CompositeMap around line 23
+componentDidMount() {
+    PlacesDataAPI.getDetails(this.props.id)
+    .then(data => {
+        console.log(data)
+        if(data !== undefined && data.meta.code === 200) {
+            this.setState({
+                loaded: true,
+                selectedPlace: data.response.venue
+            });
+        } else {
+            this.setState({
+                error: true
+            })
+        }
+    })
+    .catch(err => console.warn(err))
+}
 
     render() {
+        const { loaded, selectedPlace, error } = this.state;
+        
+    
         return ( 
             <div>
-             Content
+            {loaded && selectedPlace && (
+             <div>
+              <img src={`${selectedPlace.bestPhoto.prefix}width500${selectedPlace.bestPhoto.suffix}`} />
+               <h3><strong>Venue</strong>: <a href={selectedPlace.canonicalUrl} target='_blank'>{selectedPlace.name}</a></h3>
+              {selectedPlace.location.city && <p><strong>City</strong>: {selectedPlace.location.city}</p>}
+               <p><strong>Latitude/Longitude</strong>: {selectedPlace.location.lat}-{selectedPlace.location.lng}</p>
+               <p><strong>Rating</strong>: {selectedPlace.rating}</p>
+             </div>
+            )}
+            {error && <p style={{color: 'red'}}><strong>Cannot load any place</strong></p>}
+             
+             
             </div>
     
     
