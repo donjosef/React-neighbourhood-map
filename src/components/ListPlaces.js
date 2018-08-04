e4e879ef5be7dde595680ae8f3301adc51d26ca9
donjoe <giumontanaro@gmail.com>
2018-08-04 12:33:22 +0200

    added visual attribution to list

--- src/components/ListPlaces.js
+++ src/components/ListPlaces.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import foursquareAttributionUrl from '../imgs/powered-by-foursquare-blue.png'
 
 class ListPlaces extends Component {
     
@@ -6,16 +7,19 @@ class ListPlaces extends Component {
     render() {
         const { places, onItemClick } = this.props;
         return (
-            <ul className='listPlaces'>
-                {places.map((place, idx) => (
-                  <li 
-                     key={place.venue.id} 
-                     onClick={() => onItemClick(idx, {lat: place.venue.location.lat, lng: place.venue.location.lng})}
-                    >
-                     {place.venue.name}
-                   </li>
-                  ))}
-            </ul>
+            <div>
+              <ul className='listPlaces'>
+                  {places.map((place, idx) => (
+                    <li 
+                       key={place.venue.id} 
+                       onClick={() => onItemClick(idx, {lat: place.venue.location.lat, lng: place.venue.location.lng})}
+                      >
+                       {place.venue.name}
+                     </li>
+                    ))}
+              </ul>
+              <img src={foursquareAttributionUrl} className='foursquare-logo-list'/>
+            </div>
         )
     }
 }
